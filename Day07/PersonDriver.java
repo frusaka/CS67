56a770f21562b523c976a6be26815d6e2f937d92
frusaka <fredrick.rusaka@gmail.com>
2026-09-24 09:30:35 -0500

    Remove other files

--- Day07/PersonDriver.java
+++
@@ -1,6 +0,0 @@
-public class PersonDriver {
-    public static void main(String[] args) {
-        Person frederic = new Person("Frederic", new Date(1, 7, 2007));
-        System.out.println(frederic);
-    }
-}
